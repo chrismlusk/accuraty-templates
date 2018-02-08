9212a4fb3e0660a936008ece4053ad6916a7c9f4
Chris Lusk <chris.m.lusk@gmail.com>
2018-02-08 13:31:18 -0600

    add starter scripts

--- DNN/skin/assets/js/scripts.js
+++ DNN/skin/assets/js/scripts.js
@@ -3,7 +3,45 @@
 
   $(function () {
 
-    // DOM is ready. Code away!
+    //
+    // Constants
+    // =========
+    //
+    // Defined variables that are global and permanent.
+    // ------------------------------------------------------------------------
+
+    var BODY = $('#Body');
+
+
+
+
+    //
+    // Is DNN Persona Bar active?
+    // ==========================
+    //
+    // As of now, DNN 9 does not apply any classes to the `<body>` when the
+    // Persona Bar is active (it did previously with the Control Bar). The
+    // `.dnnEditState` class is added when you enter Edit mode, but that's it.
+    //
+    // 1. DNN adds the Persona Bar via an iframe, and `.personalBarContainer`
+    //    is the class applied to the iframe container. Notice that is
+    //    "personal" with an "L" and not "persona"? Hooray for consistency...
+    //
+    // 2. To check if the element exists, use the `length` property. This will
+    //    return `0` if it does not exist and the statement will fail.
+    //
+    // 3. Add custom `.hasDnnPersonaBar` class to the `<body>` if the element
+    //    does exist. Note: Use camel case to match other DNN-injected classes.
+    //
+    // @author Chris Lusk
+    // @date 3/1/2017
+    // ------------------------------------------------------------------------
+
+    var dnnPersonaBar = $('.personalBarContainer'); // 1
+
+    if (dnnPersonaBar.length) { // 2
+      BODY.addClass('hasDnnPersonaBar'); // 3
+    }
 
 
 
@@ -54,8 +92,8 @@
       // If scrolled down far enough, hide the element
       if ( st > lastScrollTop && st > hiddenPoint ){
         element.addClass('is-hidden');
-      } else if ( st < visibilePoint ) {
-          // Else if scrolled up past the `visibilePoint`, remove hidden class
+      } else if ( st < visiblePoint ) {
+          // Else if scrolled up past the `visiblePoint`, remove hidden class
           element.removeClass('is-hidden');
       }
 
