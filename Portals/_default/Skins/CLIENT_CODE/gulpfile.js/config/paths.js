87f2136bbf52596e51247f524911902f3a883725
Chris Lusk <chris.m.lusk@gmail.com>
2019-02-14 10:56:26 -0600

    use backticks for all string for consistency

--- Portals/_default/Skins/CLIENT_CODE/gulpfile.js/config/paths.js
+++ Portals/_default/Skins/CLIENT_CODE/gulpfile.js/config/paths.js
@@ -2,43 +2,43 @@ const { name } = require('./project');
 
 module.exports = {
   bootstrapJs: {
-    src: './node_modules/bootstrap/dist/js/bootstrap.bundle.min.js',
-    dest: './public/js/',
+    src: `./node_modules/bootstrap/dist/js/bootstrap.bundle.min.js`,
+    dest: `./public/js/`,
   },
   flickityCss: {
-    src: './node_modules/flickity/dist/flickity.min.css',
-    dest: './public/css/',
+    src: `./node_modules/flickity/dist/flickity.min.css`,
+    dest: `./public/css/`,
   },
   flickityJs: {
-    src: './node_modules/flickity/dist/flickity.pkgd.min.js',
-    dest: './public/js/',
+    src: `./node_modules/flickity/dist/flickity.pkgd.min.js`,
+    dest: `./public/js/`,
   },
   fonts: {
-    src: './src/fonts/*',
-    dest: './public/fonts/',
+    src: `./src/fonts/*`,
+    dest: `./public/fonts/`,
   },
   icons: {
-    src: './src/icons/*',
-    dest: './public/icons/',
+    src: `./src/icons/*`,
+    dest: `./public/icons/`,
   },
   images: {
-    src: './src/images/**/*.{jpg,jpeg,png,gif,svg}',
-    dest: './public/images/',
+    src: `./src/images/**/*.{jpg,jpeg,png,gif,svg}`,
+    dest: `./public/images/`,
   },
   styles: {
-    src: './src/scss/**/*.scss',
-    dest: './public/css',
+    src: `./src/scss/**/*.scss`,
+    dest: `./public/css`,
   },
   skinLayoutStyles: {
     src: `./src/scss/*.scss`,
-    dest: './',
+    dest: `./`,
   },
   containerStyles: {
     src: `./src/scss/Containers/*.scss`,
     dest: `../../Containers/${name}/`,
   },
   ednStyles: {
-    src: './src/scss/EasyDNNsolutions/EasyDNNnews.scss',
+    src: `./src/scss/EasyDNNsolutions/EasyDNNnews.scss`,
     dest: `../../../../DesktopModules/EasyDNNnews/Templates/_default/${name}/`,
   },
   accuratyContainerStyles: {
@@ -46,7 +46,7 @@ module.exports = {
     dest: `../../Containers/Accuraty/`,
   },
   scripts: {
-    src: './src/js/**/*.js',
-    dest: './public/js/',
+    src: `./src/js/**/*.js`,
+    dest: `./public/js/`,
   },
 };
