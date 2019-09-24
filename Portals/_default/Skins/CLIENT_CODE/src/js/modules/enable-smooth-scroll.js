82bb7a7c0b1e87f6a435cbfc0f3ec2b5f09e0482
Chris Lusk <chris.m.lusk@gmail.com>
2019-09-24 16:02:24 -0500

    update for accessibility issues

--- Portals/_default/Skins/CLIENT_CODE/src/js/modules/enable-smooth-scroll.js
+++ Portals/_default/Skins/CLIENT_CODE/src/js/modules/enable-smooth-scroll.js
@@ -4,44 +4,63 @@
 //
 // How to use this module:
 //
-// Import it where you want the functionality to be active, and invoke it.
-// (Example: If site-wide, `main.js`. If only on homepage links, `home.js`.)
-//
 //   `import enableSmoothScroll from './modules/enable-smooth-scroll';`
 //   `enableSmoothScroll();`
 // ----------------------------------------------------------------------------
 
-function enableSmoothScroll() {
+import isReducedMotion from '../utils/is-reduced-motion';
+import getElementStyle from '../utils/get-element-style';
+
+function enableSmoothScroll(providedOffset = 50) {
   const links = document.querySelectorAll('a[href^="#"]');
   if (!links.length) return;
 
-  const distanceToTop = el => Math.floor(el.getBoundingClientRect().top);
+  // https://developers.google.com/web/updates/2019/03/prefers-reduced-motion
+  const prefersReducedMotion = isReducedMotion();
+
+  // Space between top of the window and the anchor.
+  let offset = providedOffset;
+
+  // If the header is fixed, add its height to the `offset` value.
+  const header = document.querySelector('.header');
+  if (getElementStyle(header, 'position') === 'fixed') {
+    offset += header.getBoundingClientRect().height;
+  }
+
+  function distanceToTop(node) {
+    return Math.floor(node.getBoundingClientRect().top) - offset;
+  }
 
   function handleAnchorLink(event) {
     event.preventDefault();
 
-    const targetID = event.target.getAttribute('href');
-    const targetAnchor = document.querySelector(targetID);
-    if (!targetAnchor) return;
+    const id = event.target.getAttribute('href');
+    const anchor = document.querySelector(id);
+    if (!anchor) return;
 
     // https://developer.mozilla.org/Web/API/ScrollToOptions
     const scrollOptions = {
-      top: distanceToTop(targetAnchor),
+      top: distanceToTop(anchor),
       left: 0,
-      behavior: 'smooth',
+      behavior: prefersReducedMotion ? 'auto' : 'smooth',
     };
 
     // https://developer.mozilla.org/Web/API/Window/scrollBy
     window.scrollBy(scrollOptions);
 
-    const checkIfDone = setInterval(function() {
+    // To prevent accessibility issues, we handle the focus change by setting
+    // the `tabIndex` and focusing on the element. Then to adhere to standard
+    // behavior, add the hash to the end of the URL. Because changing focus
+    // too early breaks everything, use an interval to make sure the element
+    // has scrolled to top or we are at the bottom of the page.
+    const checkIfDone = setInterval(() => {
       const atBottom =
         window.innerHeight + window.pageYOffset >=
         document.body.offsetHeight - 2;
-      if (distanceToTop(targetAnchor) === 0 || atBottom) {
-        targetAnchor.tabIndex = '-1';
-        targetAnchor.focus();
-        window.history.pushState('', '', targetID);
+      if (distanceToTop(anchor) === 0 || atBottom) {
+        anchor.tabIndex = '-1';
+        anchor.focus();
+        window.history.pushState('', '', id);
         clearInterval(checkIfDone);
       }
     }, 100);
