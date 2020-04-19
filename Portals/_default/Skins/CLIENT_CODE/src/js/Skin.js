734fa0cab233ec02b6a48e0dcc78f58f47afaf20
Chris Lusk <chris.m.lusk@gmail.com>
2020-04-19 14:55:55 -0500

    add demo component for reference

--- Portals/_default/Skins/CLIENT_CODE/src/js/Skin.js
+++ Portals/_default/Skins/CLIENT_CODE/src/js/Skin.js
@@ -1,7 +1,19 @@
-import skipToContent from './modules/skip-to-content';
-import transformHeader from './modules/transform-header';
-import enableDropdownOnHover from './modules/enable-dropdown-on-hover';
+import 'bootstrap';
+
+import skipToContent from './lib/skip-to-content';
+import smoothScroll from './lib/smooth-scroll';
+
+import { Global as App } from './app';
+
+document.documentElement.classList.remove('no-js');
 
 skipToContent();
-transformHeader();
-enableDropdownOnHover();
+smoothScroll();
+
+const app = new App();
+app.init();
+
+// This is just here to confirm things are working.
+// You will want to delete this.
+import Demo from './components/Demo';
+new Demo(document.documentElement); // eslint-disable-line
--- Portals/_default/Skins/CLIENT_CODE/src/js/app/config.js
+++ Portals/_default/Skins/CLIENT_CODE/src/js/app/config.js
@@ -1,11 +1,9 @@
 import Header from '../components/Header';
 import NavMenu from '../components/NavMenu';
-import WelcomeMessage from '../components/WelcomeMessage';
 
 export const GlobalComponents = {
   Header,
   NavMenu,
-  WelcomeMessage,
 };
 
 export const DataAttribute = {
